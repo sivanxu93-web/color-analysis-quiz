f9353330cd495af7213bbb661a7cf537becce5eb
xuxiwu <>
2026-04-27 16:23:59 +0800

    Fix: Add external image hostname for next/image to fix 500 error in dev and prod.

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pub-6ecbfcbc11a346deadf5251217b00520.r2.dev',
+            },
+        ],
+    },
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
